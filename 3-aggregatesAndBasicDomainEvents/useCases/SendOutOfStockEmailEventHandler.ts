karpov-kir/architecture-patterns-with-python

All implementations

3-aggregatesAndBasicDomainEvents/useCases/SendOutOfStockEmailEventHandler.ts
@@ -1,0 +1,11 @@
+import { OutOfStockEvent } from '../domain/events/OutOfStockEvent';
+import { EmailServicePort } from '../ports/EmailServicePort';
+import { EventHandler } from '../shared/Event';
+
+export class SendOutOfStockEmailEventHandler implements EventHandler<OutOfStockEvent> {
+  constructor(private readonly emailService: EmailServicePort) {}
+
+  public handle(event: OutOfStockEvent): Promise<void> {
+    return this.emailService.sendEmail('admin@test.com', `Out of stock: ${event.props.sku}`);
+  }
+}

4-eventDriven/entrypoints/webServer/controllers/AddBatchController.ts
@@ -1,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { AddBatchCommand } from '../../../domain/commands/AddBatchCommand';
+import { EventBusPort } from '../../../ports/EventBusPort';
+import { Controller } from './Controller';
+
+export class AddBatchController implements Controller {
+  constructor(private readonly internalEventBus: EventBusPort) {}
+
+  async handle(
+    request: FastifyRequest<{
+      Body: {
+        reference: string;
+        sku: string;
+        purchasedQuantity: number;
+        eta?: string;
+      };
+    }>,
+    reply: FastifyReply,
+  ) {
+    await this.internalEventBus.publish(
+      new AddBatchCommand({
+        reference: request.body.reference,
+        sku: request.body.sku,
+        purchasedQuantity: request.body.purchasedQuantity,
+        eta: request.body.eta ? new Date(request.body.eta) : undefined,
+      }),
+    );
+
+    reply.code(204).send();
+  }
+}

4-eventDriven/entrypoints/webServer/controllers/AllocateController.ts
@@ -1,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { AllocateCommand } from '../../../domain/commands/AllocateCommand';
+import { EventBusPort } from '../../../ports/EventBusPort';
+import { Controller } from './Controller';
+
+export class AllocateController implements Controller {
+  constructor(private readonly internalEventBus: EventBusPort) {}
+
+  async handle(
+    request: FastifyRequest<{
+      Body: {
+        orderId: string;
+        sku: string;
+        quantity: number;
+      };
+    }>,
+    reply: FastifyReply,
+  ) {
+    await this.internalEventBus.publish(
+      new AllocateCommand({
+        orderId: request.body.orderId,
+        sku: request.body.sku,
+        quantity: request.body.quantity,
+      }),
+    );
+
+    reply.code(204).send();
+  }
+}

karpov-kir/architecture-patterns-with-python-in-typescript

3-aggregatesAndBasicDomainEvents/useCases/SendOutOfStockEmailEventHandler.ts
@@ -1,0 +1,11 @@
+import { OutOfStockEvent } from '../domain/events/OutOfStockEvent';
+import { EmailServicePort } from '../ports/EmailServicePort';
+import { MessageHandler } from '../shared/Message';
+
+export class SendOutOfStockEmailEventHandler implements MessageHandler<OutOfStockEvent> {
+  constructor(private readonly emailService: EmailServicePort) {}
+
+  public handle(event: OutOfStockEvent): Promise<void> {
+    return this.emailService.sendEmail('admin@test.com', `Out of stock: ${event.props.sku}`);
+  }
+}

4-eventDriven/entrypoints/webServer/controllers/AddBatchController.ts
@@ -1,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { AddBatchCommand } from '../../../domain/commands/AddBatchCommand';
+import { MessageBusPort } from '../../../ports/MessageBusPort';
+import { Controller } from './Controller';
+
+export class AddBatchController implements Controller {
+  constructor(private readonly internalMessageBus: MessageBusPort) {}
+
+  async handle(
+    request: FastifyRequest<{
+      Body: {
+        reference: string;
+        sku: string;
+        purchasedQuantity: number;
+        eta?: string;
+      };
+    }>,
+    reply: FastifyReply,
+  ) {
+    await this.internalMessageBus.publish(
+      new AddBatchCommand({
+        reference: request.body.reference,
+        sku: request.body.sku,
+        purchasedQuantity: request.body.purchasedQuantity,
+        eta: request.body.eta ? new Date(request.body.eta) : undefined,
+      }),
+    );
+
+    reply.code(204).send();
+  }
+}

4-eventDriven/entrypoints/webServer/controllers/AllocateController.ts
@@ -1,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { AllocateCommand } from '../../../domain/commands/AllocateCommand';
+import { MessageBusPort } from '../../../ports/MessageBusPort';
+import { Controller } from './Controller';
+
+export class AllocateController implements Controller {
+  constructor(private readonly internalMessageBus: MessageBusPort) {}
+
+  async handle(
+    request: FastifyRequest<{
+      Body: {
+        orderId: string;
+        sku: string;
+        quantity: number;
+      };
+    }>,
+    reply: FastifyReply,
+  ) {
+    await this.internalMessageBus.publish(
+      new AllocateCommand({
+        orderId: request.body.orderId,
+        sku: request.body.sku,
+        quantity: request.body.quantity,
+      }),
+    );
+
+    reply.code(204).send();
+  }
+}